453e8e3bb91ed2a30ee05b72cb383933cbf622fe
Sheldon <xt2287461052@sina.com>
2023-02-27 15:12:43 +0800

    second commit

--- git-test/src/main/java/priv/xt/git/GitTest.java
+++ git-test/src/main/java/priv/xt/git/GitTest.java
@@ -3,5 +3,6 @@ package priv.xt.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("hello,git2");
     }
 }
